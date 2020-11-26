e9b5dff85e41c2fd8f2f834b2c30fccb6f44309d
Katrina Beske <kbeske@Katrinas-MBP.fios-router.home>
2020-11-25 20:07:09 -0500

    messing around with percentages of closed restaurants by cuisine

--- yelp_covid.sql
+++ yelp_covid.sql
@@ -1,21 +1,27 @@
+create schema yelp_covid;
 use yelp_covid;
 
 
 create view closed_restaurant as
 select * 
-from business_data
+from restaurant
 where is_closed = 1;
 
+select count(*) from closed_restaurant; -- 79 total closed restaurants
+select count(*) from restaurant; -- 4353
+
+select round((sum(is_closed) / count(*)) * 100, 2)
+from restaurant; -- 1.81% of restaurants are closed (not as bad as I thought)
 
 -- Average review rating for restaurants that closed was lower than the ones open
 
 select avg(star_rating)
 from restaurant
-where num_reviews >= 100;
+where num_reviews >= 100; -- 3.723
 
 select avg(star_rating)
 from closed_restaurant
-where num_reviews >= 100;
+where num_reviews >= 100; -- 3.595
 
 -- determine type of cuisine in closed
 select cuisine, count(cuisine)
@@ -25,7 +31,106 @@ group by cuisine;
 -- 27 total "american" cuisine restaurants closed 
 select count(*)
 from closed_restaurant
-where cuisine like '%, american%' or cuisine like 'american%';
+where cuisine like '%, american%' or cuisine like 'american%'; -- 27
+
+select count(*)
+from restaurant
+where cuisine like '%, american%' or cuisine like 'american%'; -- 544
+
+select round((sum(is_closed) / count(*)) * 100, 2)
+from restaurant
+where cuisine like '%, american%' or cuisine like 'american%'; -- 4.96
+
+-- italian cuisine
+select count(*)
+from closed_restaurant
+where cuisine like '%, italian%' or cuisine like 'italian%'; -- 8
+
+select count(*)
+from restaurant
+where cuisine like '%, italian%' or cuisine like 'italian%'; -- 263
+
+select round((sum(is_closed) / count(*)) * 100, 2)
+from restaurant
+where cuisine like '%, italian%' or cuisine like 'italian%'; -- 3.04
+
+-- breakfast
+select count(*)
+from closed_restaurant
+where cuisine like '%, breakfast%' or cuisine like 'breakfast%'; -- 14
+
+select count(*)
+from restaurant
+where cuisine like '%, breakfast%' or cuisine like 'breakfast%'; -- 301
+
+select round((sum(is_closed) / count(*)) * 100, 2)
+from restaurant
+where cuisine like '%, breakfast%' or cuisine like 'breakfast%'; -- 4.65
+
+-- bars (any bars, not just sports bars or dive bars)
+select count(*)
+from closed_restaurant
+where cuisine like '%, bars%' or cuisine like '% bars%' or cuisine like 'bars%'; -- 29
+
+select count(*)
+from restaurant
+where cuisine like '%, bars%' or cuisine like '% bars%' or cuisine like 'bars%'; -- 750
+
+select round((sum(is_closed) / count(*)) * 100, 2)
+from restaurant
+where cuisine like '%, bars%' or cuisine like '% bars%' or cuisine like 'bars%'; -- 3.87
+
+-- mexican cuisine
+select count(*)
+from closed_restaurant
+where cuisine like '%, mexican%' or cuisine like 'mexican%'; -- 1
+
+select count(*)
+from restaurant
+where cuisine like '%, mexican%' or cuisine like 'mexican%'; -- 171
+
+select round((sum(is_closed) / count(*)) * 100, 2)
+from restaurant
+where cuisine like '%, mexican%' or cuisine like 'mexican%'; -- .58
+
+-- food trucks
+select count(*)
+from closed_restaurant
+where cuisine like '%food trucks%'; -- 0
+
+select count(*)
+from restaurant
+where cuisine like '%food trucks%'; -- 62
+
+select round((sum(is_closed) / count(*)) * 100, 2)
+from restaurant
+where cuisine like '%food trucks%'; -- 0
+
+-- coffee and tea
+select count(*)
+from closed_restaurant
+where cuisine like '%coffee & tea%'; -- 8
+
+select count(*)
+from restaurant
+where cuisine like '%coffee & tea%'; -- 632
+
+select round((sum(is_closed) / count(*)) * 100, 2)
+from restaurant
+where cuisine like '%coffee & tea%'; -- 1.27
+
+-- japanese
+select count(*)
+from closed_restaurant
+where cuisine like '%japanese%'; -- 4
+
+select count(*)
+from restaurant
+where cuisine like '%japanese%'; -- 108
+
+select round((sum(is_closed) / count(*)) * 100, 2)
+from restaurant
+where cuisine like '%japanese%'; -- 3.70
 
 -- How many closed restaurants are in the same zip codes as colleges/universities 
 
