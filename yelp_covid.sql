59296ea192f44a3887ad1ded59e0f89ac7d12364
kricaurte <60709721+kricaurte@users.noreply.github.com>
2020-11-30 14:22:20 -0500

    Add files via upload

--- yelp_covid.sql
+++ yelp_covid.sql
@@ -1,27 +1,21 @@
-create schema yelp_covid;
 use yelp_covid;
 
 
 create view closed_restaurant as
 select * 
-from restaurant
+from business_data
 where is_closed = 1;
 
-select count(*) from closed_restaurant; -- 79 total closed restaurants
-select count(*) from restaurant; -- 4353
-
-select round((sum(is_closed) / count(*)) * 100, 2)
-from restaurant; -- 1.81% of restaurants are closed (not as bad as I thought)
 
 -- Average review rating for restaurants that closed was lower than the ones open
 
 select avg(star_rating)
 from restaurant
-where num_reviews >= 100; -- 3.723
+where num_reviews >= 100;
 
 select avg(star_rating)
 from closed_restaurant
-where num_reviews >= 100; -- 3.595
+where num_reviews >= 100;
 
 -- determine type of cuisine in closed
 select cuisine, count(cuisine)
@@ -31,106 +25,7 @@ group by cuisine;
 -- 27 total "american" cuisine restaurants closed 
 select count(*)
 from closed_restaurant
-where cuisine like '%, american%' or cuisine like 'american%'; -- 27
-
-select count(*)
-from restaurant
-where cuisine like '%, american%' or cuisine like 'american%'; -- 544
-
-select round((sum(is_closed) / count(*)) * 100, 2)
-from restaurant
-where cuisine like '%, american%' or cuisine like 'american%'; -- 4.96
-
--- italian cuisine
-select count(*)
-from closed_restaurant
-where cuisine like '%, italian%' or cuisine like 'italian%'; -- 8
-
-select count(*)
-from restaurant
-where cuisine like '%, italian%' or cuisine like 'italian%'; -- 263
-
-select round((sum(is_closed) / count(*)) * 100, 2)
-from restaurant
-where cuisine like '%, italian%' or cuisine like 'italian%'; -- 3.04
-
--- breakfast
-select count(*)
-from closed_restaurant
-where cuisine like '%, breakfast%' or cuisine like 'breakfast%'; -- 14
-
-select count(*)
-from restaurant
-where cuisine like '%, breakfast%' or cuisine like 'breakfast%'; -- 301
-
-select round((sum(is_closed) / count(*)) * 100, 2)
-from restaurant
-where cuisine like '%, breakfast%' or cuisine like 'breakfast%'; -- 4.65
-
--- bars (any bars, not just sports bars or dive bars)
-select count(*)
-from closed_restaurant
-where cuisine like '%, bars%' or cuisine like '% bars%' or cuisine like 'bars%'; -- 29
-
-select count(*)
-from restaurant
-where cuisine like '%, bars%' or cuisine like '% bars%' or cuisine like 'bars%'; -- 750
-
-select round((sum(is_closed) / count(*)) * 100, 2)
-from restaurant
-where cuisine like '%, bars%' or cuisine like '% bars%' or cuisine like 'bars%'; -- 3.87
-
--- mexican cuisine
-select count(*)
-from closed_restaurant
-where cuisine like '%, mexican%' or cuisine like 'mexican%'; -- 1
-
-select count(*)
-from restaurant
-where cuisine like '%, mexican%' or cuisine like 'mexican%'; -- 171
-
-select round((sum(is_closed) / count(*)) * 100, 2)
-from restaurant
-where cuisine like '%, mexican%' or cuisine like 'mexican%'; -- .58
-
--- food trucks
-select count(*)
-from closed_restaurant
-where cuisine like '%food trucks%'; -- 0
-
-select count(*)
-from restaurant
-where cuisine like '%food trucks%'; -- 62
-
-select round((sum(is_closed) / count(*)) * 100, 2)
-from restaurant
-where cuisine like '%food trucks%'; -- 0
-
--- coffee and tea
-select count(*)
-from closed_restaurant
-where cuisine like '%coffee & tea%'; -- 8
-
-select count(*)
-from restaurant
-where cuisine like '%coffee & tea%'; -- 632
-
-select round((sum(is_closed) / count(*)) * 100, 2)
-from restaurant
-where cuisine like '%coffee & tea%'; -- 1.27
-
--- japanese
-select count(*)
-from closed_restaurant
-where cuisine like '%japanese%'; -- 4
-
-select count(*)
-from restaurant
-where cuisine like '%japanese%'; -- 108
-
-select round((sum(is_closed) / count(*)) * 100, 2)
-from restaurant
-where cuisine like '%japanese%'; -- 3.70
+where cuisine like '%, american%' or cuisine like 'american%';
 
 -- How many closed restaurants are in the same zip codes as colleges/universities 
 
@@ -148,32 +43,6 @@ from colleges_and_universities
 where address like ', street,  %0%';
 
 -- average rating per cuisine 
-
-
-
--- shows the difference in property costs, and therefore, the rent costs of different regions in boston
-create view downtown_property as
-select zip_code, round(avg(value)) as 'average_property_value'
-from property_assessment
-where zip_code in ('02108', '02109', '02110', '02210', '02114', '02113', '02116', '02118')
-group by zip_code
-order by average_property_value desc;
-
-create view non_downtown_property as
-select zip_code, round(avg(value)) as 'average_property_value'
-from property_assessment
-where zip_code in ('02115', '02129', '02128')
-group by zip_code
-order by average_property_value desc;
-
-select round(avg(average_property_value)) as 'average_value'
-from downtown_property;
-
-select round(avg(average_property_value)) as 'average_value'
-from non_downtown_property;
-
-
-
 
 -- what percentage of each cuisine was closed post-covid 
 -- which cuisine type had the highest percentage of closed restaurants
